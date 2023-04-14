bc51fecf65abc05f4acbdfdbf17f601a5b8ee2cb
Jimmy Wu <jameswu49@gmail.com>
2023-04-14 23:29:21 +0000

    test

--- sprints/src/index.js
+++ sprints/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
+

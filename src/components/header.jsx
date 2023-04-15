1c43b66d5d2241e1ed99ab4d051ab16fcca6bece
Jimmy Wu <jameswu49@gmail.com>
2023-04-15 00:02:03 +0000

    hash

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <header className="flex py-5 justify-start items-center print:ml-10 md:ml-60 lg:ml-80">
-            <h1 className="text-base font-bold">Name: ____________________</h1>
+            <h1 className="text-base font-bold w-[50%]">Name: ____________________</h1>
             <h1 className="text-base ml-40 lg:ml-52 2xl:ml-72 font-bold justify-end">Date: ____________________</h1>
         </header>
     )
